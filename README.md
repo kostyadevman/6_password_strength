488af2a0edbf351e6897eae9e76f84984860c1a4
kostya <kostra@localdomain>
2018-04-22 18:36:50 +0300

    Initial commit

--- README.md
+++ README.md
@@ -1,6 +1,16 @@
 # Password Strength Calculator
 
-[TODO. There will be project description]
+Get estimate the password you enter. Score from 0 (weak)to 10(strong). The haiest estimate can be reached if the passwod not in the most [common password list](https://github.com/danielmiessler/SecLists/tree/master/Passwords/Common-Credentials).
+
+The script requires the installed Python interpreter version 3.5
+
+Example of script launch on Linux, Python 3.5:
+
+````bash
+$ python password_strength.py --dict [ path to dictionary ]
+Enter the password: your_super_puper_password
+Password estimate:  6
+````
 
 # Project Goals
 
--- password_strength.py
+++ password_strength.py
@@ -1,81 +1,77 @@
 import os
-import sys
-from string import punctuation
-from string import ascii_lowercase
-from string import ascii_uppercase
-from string import digits
+from string import punctuation, digits
+from string import ascii_lowercase, ascii_uppercase
+import argparse
 
 
+def get_argument():
+    parser = argparse.ArgumentParser('Password strength')
+    parser.add_argument('--dict', type=str)
+    return parser.parse_args()
 
-MIN_PASS_LENGTH = 8
-special_characters = set(punctuation)
 
-def get_password_strength(password):
+def get_dictionary(common_used_passwords_file):
+    if not common_used_passwords_file:
+        return None
+    if not os.path.isfile(common_used_passwords_file):
+        return None
+    with open(common_used_passwords_file, 'r') as password_file:
+        common_used_passwords = password_file.readlines()
+    return common_used_passwords
+
+
+def get_password_strength(password, common_used_passwords):
+    min_pass_length = 8
+    special_characters = set(punctuation)
+
     pass_estimate = 0
     if password != '':
         pass_estimate += 1
 
-    if len(password) < MIN_PASS_LENGTH:
-        pass_estimate = 1
-        return pass_estimate
-    elif len(password) >= MIN_PASS_LENGTH:
+    if len(password) > min_pass_length:
+        pass_estimate += 2
+
+    if any(char.islower() for char in password) and \
+        any(char.isupper() for char in password):
         pass_estimate += 1
 
-
-    if any(chracter.islower() for chracter in password) and \
-        any (character.isupper() for character in password):
+    if any(char.isdigit() for char in password):
         pass_estimate += 1
 
-    if any(character.isdigit() for character in password):
+    if any(char in special_characters for char in password):
         pass_estimate += 1
-
-
-    if any(character in special_characters for character in password):
-        pass_estimate += 1
-
-
-
 
     # more than 2 identical characters
     identical_character_count = 0
-    for char_iter in range(0,len(password)-2):
-        if  password[char_iter] == password[char_iter+1] == password[char_iter+2]:
+    for char_number in range(0, len(password)-2):
+        if (password[char_number] == password[char_number+1] ==
+                password[char_number + 2]):
             identical_character_count += 1
     if identical_character_count == 0:
             pass_estimate += 1
 
     # more than 2 consecutive characters
     consecutive_character_count = 0
-    for char_ite in range (0, len(password)-2):
-        three_consec_char = password[char_ite:char_ite+3]
-        if three_consec_char in ascii_lowercase and \
-                three_consec_char in ascii_uppercase and \
+    for char_number in range(0, len(password)-2):
+        three_consec_char = password[char_number:char_number + 3]
+        if three_consec_char in ascii_lowercase or \
+                three_consec_char in ascii_uppercase or \
                 three_consec_char in digits:
             consecutive_character_count += 1
     if consecutive_character_count == 0:
         pass_estimate += 1
 
-
-    # Commonly used passwords
-    if os.path.exists('john.txt'):
-        with open('john.txt', 'r') as input_file:
-            common_used_passwords = input_file.read()
-        if password in common_used_passwords:
-            pass_estimate = 1
-        else:
-            pass_estimate += 1
-    else:
-        print('The file with commonly used passwords ("john.txt") doesn\'t exitst')
-        return None
-
+    if common_used_passwords:
+        if password not in common_used_passwords:
+            pass_estimate += 2
 
     return pass_estimate
 
 
 if __name__ == '__main__':
-    pass
+    args = get_argument()
+    print(args.dict)
+    dictionary = get_dictionary(args.dict)
     password = input("Enter the password: ")
-    pass_estimate = get_password_strength(password)
+    pass_estimate = get_password_strength(password, dictionary)
     print('Password estimate: ', pass_estimate)
-
-
